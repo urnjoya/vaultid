f2e48af1c01e8b51d799b4b05c8ace6cb7ee5300
Joya <privategafbusiness@gmail.com>
2025-03-18 22:09:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // element
 let add = document.getElementById("input_add");
 let fetch = document.getElementById("input_fetch");
+let body = document.getElementsByTagName("body")[0];
 // global function
 function getObjStore(db, storeName, mode) {
     let transaction = db.transaction([storeName], mode);
@@ -44,15 +45,18 @@ request.onsuccess = function (event) {
     getRequest.onsuccess = function () {
         let user = getRequest.result;
         if (user) {
+            // Login
             console.log("User Found:", user);
             let db = event.target.result;
             let store = getObjStore(db, "users", "readonly");
             let getRequest = store.get(1);
             fetch.innerHTML = `
                 <h2 id="heading">Welcome back ${user.name}</h2> 
-                <input type="password" id="pass" placeholder="enter password" required>
+                <input type="password" id="pass" placeholder="Enter your password" required>
                 <button id="btn">Login</button>
             `;
+            body.classList.add("body-theme-0");
+            fetch.classList.add("login-container");
             let pass = document.getElementById("pass");
             let heading = document.getElementById("heading");
             let btn = document.getElementById("btn");
@@ -65,55 +69,78 @@ request.onsuccess = function (event) {
                     let manual_pass = pass.value;
                     if (server_pass === manual_pass) {
                         console.log("Login Successfull");
+                        // main page
                         pass.remove();
                         heading.remove();
                         btn.remove();
+                        add.remove();
+                        fetch.remove();
+
+                        // body.className = "";
+                        body.classList.remove("body-theme-0");
+                        body.classList.add("body-theme-1");
+
                         // new page
                         let mp = document.getElementById("mainPage");
                         mp.innerHTML = `
-                        <h2 id="hd">Welcome back ${user.name}</h2>
-                        <div id="mp_nav">
-                            <strong id="home">Home</strong>
-                            <strong id="add">Add</strong>
-                            <strong id="show">Show</strong>
-                            <div id="dynMainPage"></div>
+                        <div class="marquee">
+                            <span id="hd">Welcome back ${user.name}</span>
                         </div>
-                        <button id="logout">Logout</button>
+                        <nav id="mp_nav">
+                            <div class="nav-links">
+                                <a href="#" id="home">Home</a>
+                                <a href="#" id="add">Add</a>
+                                <a href="#" id="delete">Delete</a>
+                                <a href="#" id="show">Show</a>
+                                <a href="#" id="about">About</a>
+                            </div>
+                            <button id="logout" class="logout-button">Logout</button>
+                        </nav>
+                        <div id="dynMainPage"></div>
                         `;
                         let home_nav = document.getElementById("home")
                         let add_nav = document.getElementById("add")
                         let show_nav = document.getElementById("show")
                         let dmp = document.getElementById("dynMainPage");
+                        let logout = document.getElementById("logout");
                         function cleardmp() {
                             dmp.innerHTML = "";
-                            console.log(dmp.innerHTML, " is clear");
                         }
                         function showHome() {
                             cleardmp();
                             dmp.innerHTML = `  
-                                <p> Home Page</p>
+                            <div class="">
+                                <div class="card">
+                                    <div class="floating-element"></div>
+                                    <div class="card-content"></div>
+                                </div>
+                            </div>
                             `;
-                            console.log(dmp.innerHTML, " is clear");
-
+                            dmp.classList.add("body-theme-v-0");
                         }
                         function showAdd() {
                             cleardmp();
                             dmp.innerHTML = `  
                                 <p> Add Page</p>
                             `;
-                            console.log(dmp.innerHTML, " is clear");
-
                         }
                         function showShow() {
                             cleardmp();
                             dmp.innerHTML = `  
                                 <p> Show Page</p>
                             `;
-                            console.log(dmp.innerHTML, " is clear");
+                        }
+                        function doLogout() {
+                            caches.keys().then(names => {
+                                names.forEach(name => caches.delete(name));
+                            }).then(() => {
+                                location.reload(true);
+                            });
                         }
                         home_nav.addEventListener("click", showHome);
                         add_nav.addEventListener("click", showAdd);
                         show_nav.addEventListener("click", showShow);
+                        logout.addEventListener("click", doLogout);
                     }
                     else {
                         console.log("Incorrect");
@@ -123,11 +150,15 @@ request.onsuccess = function (event) {
         } else {
             console.warn("No profile found!");
             // add data
+            // signup
             add.innerHTML = `
-                <input type="text" id="name" placeholder="enter name" required>
-                <input type="password" id="password" placeholder="enter password" required>
+                <h2> Sign Up</h2>
+                <input type="text" id="name" placeholder="Enter your name" required>
+                <input type="password" id="password" placeholder="Enter you password" required>
                 <button id="save">Save</button>
             `;
+            body.classList.add("body-theme-0");
+            add.classList.add("sign-container");
             let userName = document.getElementById("name");
             let userPassword = document.getElementById("password");
             document.getElementById("save").addEventListener("click", function () {
